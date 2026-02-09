c676fd46a9c422cd8234cad3dc0700732e140d72
viveksingh62 <vs7492403@gmail.com>
2026-02-09 21:05:49 +0530

    frontedn ready2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { SocketProvider } from "./context/socketContext.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
